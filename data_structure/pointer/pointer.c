6943d5659dc20e055982c0d3f1d2aa55aeefd0a6
unKILLableBRIAR <wnsgud3590@naver.com>
2024-02-15 11:35:51 +0900

    포인터 연습용

---
+++ data_structure/pointer/pointer.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main(){
+    int* a;
+    int* arr = malloc(sizeof(int) * 4);
+    for(int i = 0; i < 4; i++){
+        arr[i] = i * 102 + 32 / 3;
+    }
+    a = arr;
+
+    for(int i = 0; i < 4; i++){
+        printf("%d %d\n", a[i], arr[i]);
+    }
+    free(arr);
+}
